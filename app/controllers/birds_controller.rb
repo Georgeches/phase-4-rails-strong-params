b4a2deeea64ac92998740c88d6734802746591df
Georgeches <georgechesire83@gmail.com>
2023-06-23 10:35:46 +0300

    Done assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -22,4 +22,13 @@ class BirdsController < ApplicationController
     end
   end
 
+  def create
+    bird = Bird.create(bird_params)
+    render json: bird, status: :created
+  end
+
+  def bird_params
+    params.permit(:name, :species)
+  end
+
 end
